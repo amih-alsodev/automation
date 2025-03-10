d984c9a52554fb36ef0f6883b8f72cb3002f0707
AlexandrM <miheev.alexandr@alsodev.md>
2025-03-10 09:49:08 +0200

    added site credentials data, api_base.py, api_req_payloads.json, data_processing.py. added test_api_category.py with create category test

---
+++ utils/api/api_base.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Playwright
+
+
+def get_global_site_settings(playwright: Playwright, token):
+    api_request_context = playwright.request.new_context(
+        base_url="https://stage.admin.ecom.md/"
+    )
+    response = api_request_context.get(
+        "settings/global_site_settings",
+        headers={
+            "token": token,
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.ok
+
+    response_body = response.json()
+    return response_body
